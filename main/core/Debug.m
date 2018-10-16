280a185f698b03e39b08eff065ecb260b90cd897
YQGong <gongyq@pku.edu.cn>
2018-10-17 01:18:57 +0800

    add number q in debug

--- main/core/Debug.m
+++ main/core/Debug.m
@@ -1,19 +1,12 @@
 BeginPackage["core`Debug`"];
 
-Begin["`Private`"]
+Begin["`Private`"];
 
-`defaultDebugLevel=0;
 `defaultDebugf=Print;
 `defaultComputeDebugf=Null;
 `defaultTypeList={"msg","val","core"};
 
-End[]
-
-SetAttributes[Global`debugf,HoldAll];
-If[!ValueQ[Global`debugf],
-  If[$KernelID===0,
-    Global`debugf=`Private`defaultDebugf,
-    Global`debugf[___]=`Private`defaultComputeDebugf];];
+End[];
 
 
 typeList::usage = "typeList[]";
@@ -22,6 +15,16 @@ If[!ValueQ[`typeList],
   `typeList=Union[`typeList,`Private`defaultTypeList];
 ];
 
+defaultDebugLevel::usage = "defaultDebugLevel[]";
+If[!ValueQ[`defaultDebugLevel],
+  `defaultDebugLevel=0];
+
+
+SetAttributes[Global`debugf,HoldAll];
+If[!ValueQ[Global`debugf],
+  If[$KernelID===0,
+    Global`debugf=`Private`defaultDebugf,
+    Global`debugf[___]=`Private`defaultComputeDebugf];];
 
 `Private`mergeAssociations[default_,current_]:=KeySort[Merge[{default, current}, Last]];
 `Private`defaultAssociation=Association @@ (# -> `Private`defaultDebugLevel & /@ `typeList);
@@ -34,7 +37,10 @@ If[!ValueQ[Global`$verbose],
 
 debug::usage = "debug[]";
 SetAttributes[debug,HoldAll];
-debug[msg__,level_List]:=If[Global`$verbose[level[[1]]]>=level[[2]],Global`debugf[msg]];
+debug[msg__,level_List]:=If[
+  NumberQ[Global`$verbose[level[[1]]]] &&
+      Global`$verbose[level[[1]]]>=level[[2]],
+  Global`debugf[msg]];
 
 debugset::usage = "debugset[]";
 SetAttributes[debugset,HoldAll];
